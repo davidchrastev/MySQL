709e81076e25f95b5f88a4a98b6e55d31f42493c
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-11 19:34:50 +0300

    Current tasks

--- src/data_definition_datatypes_exercise/alter_minions_table_02.sql
+++ src/data_definition_datatypes_exercise/alter_minions_table_02.sql
@@ -1,5 +1,5 @@
--- ALTER TABLE `minions`.`towns`
--- CHANGE COLUMN `town_id` `id` INT NOT NULL AUTO_INCREMENT;
+-- ALTER TABLE minions.towns
+-- CHANGE COLUMN town_id id INT NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE minions
 ADD COLUMN town_id INT;
