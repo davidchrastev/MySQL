7eab44351b50b20fc5565a1becf27ceb30827a43
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-15 19:29:00 +0300

    08 Solution.

--- src/basic_crud_exercise/08_find_names_of_all_employees_by_salary_in_range.sql
+++ src/basic_crud_exercise/08_find_names_of_all_employees_by_salary_in_range.sql
@@ -3,5 +3,7 @@ USE soft_uni;
 SELECT
     first_name, last_name, job_title
 FROM employees
-WHERE (salary >= 20000 && salary <= 30000)
+WHERE salary BETWEEN 20000 and 30000
 ORDER BY employee_id;
+
+-- WHERE (salary >= 20000 && salary <= 30000)
