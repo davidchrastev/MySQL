e2095c4b3de1a34792f5c242880ea517bf688fad
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-15 18:54:16 +0300

    03 Solution.

--- src/basic_crud_exercise/03_find_salary_of_each_employee.sql
+++ src/basic_crud_exercise/03_find_salary_of_each_employee.sql
@@ -1,7 +1,10 @@
 USE soft_uni;
 
-SELECT first_name,
-       last_name,
-       salary
-FROM employees
-ORDER BY employee_id;
+SELECT
+    first_name,
+    last_name,
+    salary
+FROM
+    employees
+ORDER BY
+    employee_id;
