b8f8d3376d00496c7a829e897d4c6550d075d2d3
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-15 19:22:43 +0300

    09 Solution.

---
+++ src/basic_crud_exercise/09_find_names_of_all_employees.sql
@@ -0,0 +1,23 @@
+USE soft_uni;
+
+SELECT
+    CONCAT(
+        first_name,
+        ' ',
+        middle_name,
+        ' ',
+        last_name)
+        AS `Full Name`
+FROM
+    employees AS e
+WHERE salary IN
+      (25000,
+       14000,
+       12500,
+       23600)
+ORDER BY employee_id;
+
+-- WHERE (salary  = 25000 ||
+--       salary = 14000  ||
+--       salary = 12500  ||
+--       salary = 23600)
