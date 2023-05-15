22a4801d99f0ef68bb1558ae933b37d11e5b2575
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-15 19:24:02 +0300

    09 Solution.

--- src/basic_crud_exercise/09_find_names_of_all_employees.sql
+++ src/basic_crud_exercise/09_find_names_of_all_employees.sql
@@ -1,12 +1,10 @@
 USE soft_uni;
 
 SELECT
-    CONCAT(
-        first_name,
-        ' ',
-        middle_name,
-        ' ',
-        last_name)
+    CONCAT_WS(' ',
+        `first_name`,
+        `middle_name`,
+        `last_name`)
         AS `Full Name`
 FROM
     employees AS e
@@ -16,6 +14,14 @@ WHERE salary IN
        12500,
        23600)
 ORDER BY employee_id;
+
+--    CONCAT(
+--        first_name,
+--        ' ',
+--        middle_name,
+--        ' ',
+--        last_name)
+--        AS `Full Name`
 
 -- WHERE (salary  = 25000 ||
 --       salary = 14000  ||
