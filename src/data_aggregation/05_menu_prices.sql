3a947c89ca937b44a845de2993b696b87c8eb3d7
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-19 19:24:56 +0300

    05. Solution

--- src/data_aggregation/05_menu_prices.sql
+++ src/data_aggregation/05_menu_prices.sql
@@ -1,9 +1,8 @@
 USE restaurant;
 
-SELECT
-    category_id,
-    ROUND(AVG(price), 2) as 'Average Price',
-    ROUND(MIN(price), 2) as 'Cheapest Product',
-    ROUND(MAX(price), 2) as 'Most Expensive Product'
+SELECT category_id,
+       ROUND(AVG(price), 2) as 'Average Price',
+       ROUND(MIN(price), 2) as 'Cheapest Product',
+       ROUND(MAX(price), 2) as 'Most Expensive Product'
 FROM products
 GROUP BY category_id;
