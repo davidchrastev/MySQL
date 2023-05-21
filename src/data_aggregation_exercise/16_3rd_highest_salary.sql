ae2c7cb7b696fe601e215bc49814da7194427768
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-21 19:43:34 +0300

    16. Solution

---
+++ src/data_aggregation_exercise/16_3rd_highest_salary.sql
@@ -0,0 +1,14 @@
+USE soft_uni;
+
+SELECT `department_id` AS `dep_id`,
+       (SELECT DISTINCT `salary`
+        FROM employees
+        WHERE `department_id` = `dep_id`
+        ORDER BY `salary` DESC
+        LIMIT 2,1) AS `third_highest_salary`
+FROM employees
+GROUP BY `dep_id`
+HAVING `third_highest_salary` IS NOT NULL
+ORDER BY `dep_id`;
+
+
