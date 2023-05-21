83540070566bfffcf00090329e2097af4572724a
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-21 19:11:22 +0300

    11. Solution

---
+++ src/data_aggregation_exercise/11_average_interest.sql
@@ -0,0 +1,14 @@
+USE gringotts;
+
+SELECT `deposit_group`,
+       (CASE
+            WHEN `is_deposit_expired` = TRUE THEN 1
+            WHEN `is_deposit_expired` = FALSE THEN 0
+           END)                AS `is_deposit_expired`,
+       AVG(`deposit_interest`) AS `average_interest`
+FROM wizzard_deposits
+WHERE `deposit_start_date` > DATE('1985-01-01')
+GROUP BY `deposit_group`,
+         `is_deposit_expired`
+ORDER BY `deposit_group` DESC,
+         is_deposit_expired;
