81e01b9e7f5175f4f864e4a83f6f179b384911bb
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-21 19:46:41 +0300

    17. Solution

---
+++ src/data_aggregation_exercise/17_salary_challenge.sql
@@ -0,0 +1,12 @@
+USE soft_uni;
+
+SELECT `first_name`,
+       `last_name`,
+       `department_id`
+FROM employees AS `emp1`
+WHERE `salary` > (SELECT AVG(`salary`)
+                  FROM employees AS `emp2`
+                  WHERE `emp2`.department_id = `emp1`.department_id)
+ORDER BY `department_id`,
+         `employee_id`
+LIMIT 10;
