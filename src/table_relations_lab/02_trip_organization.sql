6073403c3b8c693ec8926782a1b96a9ec545ef88
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-23 20:06:51 +0300

    02. Solution

---
+++ src/table_relations_lab/02_trip_organization.sql
@@ -0,0 +1,7 @@
+USE `camp`;
+
+SELECT v.`driver_id`,
+       v.`vehicle_type`,
+       CONCAT_WS(' ', c.first_name, c.last_name) AS `driver_name`
+FROM vehicles v
+         INNER JOIN campers c ON v.`driver_id` = c.`id`;
