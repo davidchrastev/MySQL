3df58e307c5e9b278aa4a85d13a6be5388927754
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-23 17:35:30 +0300

    05. Solution - creating a project management DB

---
+++ src/table_relations_lab/05_project_managment_db.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE `project_management`;
+
+USE `project_management`;
+
+CREATE TABLE `clients`(
+    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `client_name` VARCHAR(100)
+);
+
+CREATE TABLE `projects` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `client_id` INT,
+    `project_lead_id` INT
+);
+
+CREATE TABLE `employees` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `first_name` VARCHAR(30) NOT NULL,
+    `last_name` VARCHAR(30) NOT NULL,
+    `project_id` INT
+);
+
+ALTER TABLE `projects`
+ADD CONSTRAINT fk_client
+FOREIGN KEY (`client_id`) REFERENCES `clients`(`id`),
+ADD CONSTRAINT fk_employee
+FOREIGN KEY (`project_lead_id`) REFERENCES `employees`(`id`);
+
+ALTER TABLE `employees`
+ADD CONSTRAINT fk_project
+FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`);
