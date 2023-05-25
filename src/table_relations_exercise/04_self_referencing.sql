8c41a11e4098f348d16dc18dd693cc4c5cc26459
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-25 21:51:34 +0300

    04. Solution

---
+++ src/table_relations_exercise/04_self_referencing.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE `school`;
+USE `school`;
+
+CREATE TABLE `teachers` (
+    `teacher_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(30),
+    `manager_id` INT NULL,
+    CONSTRAINT fk_teachers_ref_teachers
+    FOREIGN KEY (`manager_id`) REFERENCES `teachers`(`teacher_id`)
+);
+
+ALTER TABLE `teachers`
+AUTO_INCREMENT = 101;
+
+INSERT INTO teachers (`name`)
+VALUES ('John'),
+       ('Maya'),
+       ('Silvia'),
+       ('Ted'),
+       ('Mark'),
+       ('Greta');
+
+UPDATE teachers
+SET `manager_id` = 106	WHERE `teacher_id` = 102;
+UPDATE teachers
+SET	`manager_id` = 106	WHERE `teacher_id` = 103;
+UPDATE teachers
+SET	`manager_id` = 105	WHERE `teacher_id` = 104;
+UPDATE teachers
+SET	`manager_id` = 101	WHERE `teacher_id` = 105;
+UPDATE teachers
+SET	`manager_id` = 101	WHERE `teacher_id` = 106;
+
+SELECT
+    *
+FROM teachers;
+
