b9fa2c78dfb0da9569463e9118a37a45ff6fd213
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-26 18:59:42 +0300

    01. Solution

--- src/subqueries_and_joins/01_managers.sql
+++ src/subqueries_and_joins/01_managers.sql
@@ -5,6 +5,6 @@ SELECT e.`employee_id`                           AS `employee_id`,
        d.`department_id`                         AS `department_id`,
        d.`name`                                  AS `department_id`
 FROM departments d
-         LEFT JOIN employees e ON d.`manager_id` = e.`employee_id`
+         INNER JOIN employees e ON d.`manager_id` = e.`employee_id`
 ORDER BY e.`employee_id`
 LIMIT 5;
