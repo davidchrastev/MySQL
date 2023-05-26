5d629a9b1b7ecb83d207a9daa36a7f0117c5e709
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-26 18:52:05 +0300

    01. Solution

---
+++ src/subqueries_and_joins/01_managers.sql
@@ -0,0 +1,10 @@
+USE `soft_uni`;
+
+SELECT e.`employee_id`                           AS `employee_id`,
+       CONCAT_WS(' ', `first_name`, `last_name`) AS `full_name`,
+       d.`department_id`                         AS `department_id`,
+       d.`name`                                  AS `department_id`
+FROM departments d
+         LEFT JOIN employees e ON d.`manager_id` = e.`employee_id`
+ORDER BY e.`employee_id`
+LIMIT 5;
