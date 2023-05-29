a91ff5d7b1d8adf363da9337be031c94c416125e
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-29 21:24:45 +0300

    15. Solution

--- src/subqueries_and_joins_exercise/15_continents_and_currencies.sql
+++ src/subqueries_and_joins_exercise/15_continents_and_currencies.sql
@@ -1,20 +1,20 @@
 USE `geography`;
 
-WITH `usage` AS (  SELECT `continent_code`, `currency_code`, count(currency_code) AS `usage_count`
-                   FROM countries
-                   GROUP BY `continent_code`, `currency_code`
-)
-SELECT `continent_code`, `currency_code`, `usage_count` AS `currency_usage`
-FROM `usage`
-HAVING `currency_usage` > 1 AND
-        `usage_count` = (SELECT max(`usage_count`)
-                         FROM `usage` as u
-                         WHERE `usage`.continent_code = u.continent_code
-    )
-ORDER BY `continent_code`, `currency_code`;
-
-
-
+SELECT
+    c.continent_code,
+    c.currency_code,
+    COUNT(*) AS `currency_usage`
+FROM countries c
+GROUP BY c.continent_code, c.currency_code
+HAVING `currency_usage` > 1
+AND `currency_usage` = (SELECT COUNT(*) AS count_of_currencies
+                        FROM countries c2
+                        WHERE c2.continent_code = c.continent_code
+                        GROUP BY c2.currency_code
+                        ORDER BY `count_of_currencies` DESC
+                        LIMIT 1)
+ORDER BY continent_code,
+         currency_code;
 
 
 
