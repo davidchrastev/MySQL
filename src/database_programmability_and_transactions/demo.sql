7220a57a1dec5a7cb01a7097f479f05b4b00efef
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-30 18:39:05 +0300

    Functions.

--- src/database_programmability_and_transactions/demo.sql
+++ src/database_programmability_and_transactions/demo.sql
@@ -4,6 +4,19 @@ RETURNS INT
     DETERMINISTIC
 BEGIN
     RETURN 5;
-END $$;
+END$$
 
-SELECT ufn_select_5();
+
+SELECT ufn_select_5() as result_of_made_function;
+
+drop function ufn_select_5;
+
+DELIMITER $$
+CREATE FUNCTION ufn_append_before(str VARCHAR(30))
+RETURNS VARCHAR(70)
+DETERMINISTIC
+BEGIN
+    return CONCAT_WS(' ', str, ufn_select_5());
+end $$;
+
+SELECT ufn_append_before('3 + 2 e');
