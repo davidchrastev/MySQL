60a3c93567f2d62bf2daa55d204b4ca6ae9e04d3
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-06-01 21:07:05 +0300

    11. Solution

---
+++ src/database_programmability_and_transactions_exercise/11_calculating_interest.sql
@@ -0,0 +1,17 @@
+USE `soft_uni`;
+
+DELIMITER $$
+CREATE PROCEDURE usp_calculate_future_value_for_account(account_id INT, interest_per_year DECIMAL(19,4))
+BEGIN
+    SELECT
+        ah.id,
+        ah.first_name,
+        ah.last_name,
+        a.balance AS 'current_balance',
+        ufn_calculate_future_value(a.balance, interest_per_year, 5) AS balance_in_5_years
+    FROM accounts a
+             JOIN account_holders ah on ah.id = a.account_holder_id
+    WHERE a.id = account_id;
+END $$;
+
+CALL usp_calculate_future_value_for_account(1, 0.1);
