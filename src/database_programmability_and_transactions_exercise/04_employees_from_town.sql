0b80b677cab08c87b254e94054203d637b5c085b
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-06-01 19:03:55 +0300

    04. Solution

---
+++ src/database_programmability_and_transactions_exercise/04_employees_from_town.sql
@@ -0,0 +1,22 @@
+USE `soft_uni`;
+
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_from_town(`town_name` VARCHAR(50))
+BEGIN
+    SELECT
+        e.`first_name`,
+        e.`last_name`
+    FROM employees e
+             JOIN addresses a ON a.address_id = e.address_id
+             JOIN towns t ON a.town_id = t.town_id
+    WHERE t.name = `town_name`
+    ORDER BY e.`first_name`,
+             e.`last_name`,
+             e.`employee_id`;
+END $$
+
+DELIMITER ;
+
+CALL usp_get_employees_from_town('Sofia');
+
+
