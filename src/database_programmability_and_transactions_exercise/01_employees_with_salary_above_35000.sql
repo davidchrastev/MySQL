8495fc999962e70711158d39d9c25c7a54df99f5
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-06-01 18:25:41 +0300

    01. Solution

---
+++ src/database_programmability_and_transactions_exercise/01_employees_with_salary_above_35000.sql
@@ -0,0 +1,16 @@
+USE `soft_uni`;
+
+## usp_get_employees_salary_above_35000
+
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_salary_above_35000()
+BEGIN
+    SELECT `first_name`,
+           `last_name`
+    FROM `employees`
+    WHERE `salary` > 35000
+    ORDER BY `first_name`,
+             `last_name`,
+             `employee_id`;
+END $$
+DELIMITER ;
