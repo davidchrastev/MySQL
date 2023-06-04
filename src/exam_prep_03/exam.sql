c42262b39ae4be4b0f83bca1d1c44ef310c9b08d
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-06-04 19:20:52 +0300

    Exam prep.

--- src/exam_prep_03/exam.sql
+++ src/exam_prep_03/exam.sql
@@ -62,3 +62,23 @@ CREATE TABLE `orders_products`(
                                   CONSTRAINT `fk_orders_products_products` FOREIGN KEY(`product_id`)
                                       REFERENCES `products`(`id`)
 );
+
+
+INSERT INTO reviews (content, picture_url, published_at, rating)
+SELECT LEFT(p.description, 15), reverse(p.name), DATE('2010/10/10'), p.price / 8
+FROM products p
+WHERE p.id >= 5;
+
+
+UPDATE products p
+SET p.quantity_in_stock = p.quantity_in_stock - 5
+WHERE p.quantity_in_stock BETWEEN 60 AND 70;
+
+
+DELETE c FROM customers c
+                  LEFT JOIN orders o ON c.id = o.customer_id
+WHERE o.customer_id IS NULL;
+
+
+SELECT * from categories c
+ORDER BY c.name desc;
