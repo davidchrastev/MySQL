0530ad3e05cc6e38033e635c83cb8e5d309406b2
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-06-07 19:46:18 +0300

    14. Solution

--- src/database_programmability_and_transactions_exercise/14_money_transfer.sql
+++ src/database_programmability_and_transactions_exercise/14_money_transfer.sql
@@ -4,9 +4,9 @@ DELIMITER $$
 
 DROP PROCEDURE IF EXISTS usp_transfer_money$$
 
-CREATE PROCEDURE usp_transfer_money(from_account_id INT, to_account_id INT, amount DECIMAL(19,4))
+CREATE PROCEDURE usp_transfer_money(from_account_id INT, to_account_id INT, amount DECIMAL(19, 4))
 BEGIN
-    DECLARE old_balance_from DECIMAL(19,4)
+    DECLARE old_balance_from DECIMAL(19, 4)
         DEFAULT (SELECT balance
                  FROM accounts
                  WHERE id = from_account_id);
@@ -35,8 +35,10 @@ BEGIN
        amount < 0 OR
        NOT is_valid_from_account OR
        NOT is_valid_to_account
-    THEN ROLLBACK;
-    ELSE COMMIT;
+    THEN
+        ROLLBACK;
+    ELSE
+        COMMIT;
     END IF;
 END$$
 
@@ -46,4 +48,4 @@ SELECT a.id AS account_id, ah.id AS holder_id, ah.first_name, ah.last_name, a.ba
 FROM accounts AS a
          JOIN
      account_holders AS ah ON ah.id = a.account_holder_id
-WHERE a.id IN (1,2)$$
+WHERE a.id IN (1, 2)$$
